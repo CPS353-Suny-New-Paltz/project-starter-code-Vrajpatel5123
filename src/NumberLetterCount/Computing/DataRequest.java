69f41fd7200aec21f370a9dc04e738670e5ad14b
Vrajpatel5123 <vrajpatel512@gmail.com>
2025-09-18 18:04:58 -0400

    Revert "try again"
    This reverts commit 2b9135f2a08f9b9952644120a48e0bc8fae41aa6.

--- src/NumberLetterCount/Computing/DataRequest.java
+++ src/NumberLetterCount/Computing/DataRequest.java
@@ -2,18 +2,17 @@ package NumberLetterCount.Computing;
 
 public class DataRequest {
 
-	private int number;
-
 	public DataRequest(int i) {
 		// TODO Auto-generated constructor stub
-	}
-
-	public int DataRequest(int number) {
-		return this.number = number;
-	}
-	
-	public int getNumber() {
-		return number;
+		  int number;
+		
+		public DataRequest(int number) {
+			this.number = number;
+		}
+		
+		public int getNumber() {
+			return number;
+		}
 	}
 	
 	
