2b9135f2a08f9b9952644120a48e0bc8fae41aa6
Vrajpatel5123 <vrajpatel512@gmail.com>
2025-09-18 17:57:35 -0400

    try again

--- src/NumberLetterCount/Computing/DataRequest.java
+++ src/NumberLetterCount/Computing/DataRequest.java
@@ -2,17 +2,18 @@ package NumberLetterCount.Computing;
 
 public class DataRequest {
 
+	private int number;
+
 	public DataRequest(int i) {
 		// TODO Auto-generated constructor stub
-		  int number;
-		
-		public DataRequest(int number) {
-			this.number = number;
-		}
-		
-		public int getNumber() {
-			return number;
-		}
+	}
+
+	public int DataRequest(int number) {
+		return this.number = number;
+	}
+	
+	public int getNumber() {
+		return number;
 	}
 	
 	
