1d7583c868ebb2d3c42b7ad59908db2f95110bbd
Vrajpatel5123 <vrajpatel512@gmail.com>
2025-09-19 16:46:21 -0400

    First attempt

---
+++ src/NumberLetterCount/Fetching/FetchApi.java
@@ -0,0 +1,31 @@
+package NumberLetterCount.Fetching;
+
+
+import NumberLetterCount.DataStoring.Delimiters;
+import project.annotations.NetworkAPI;
+
+@NetworkAPI
+public interface FetchApi {
+	
+	int insertRequest(FetchRequest fetchRequest);
+	
+	InputSource getInputSource();
+	
+	OutputResult getOutputResult();
+	
+	Delimiters getDelimiters();
+	
+	PassData InputSource();
+	
+	Display DisplayIt();
+	 
+	
+//	String setInputSource(String inputSource);
+//	
+//	String setOutputResult(String outputResult);
+//	
+//	String setDelimiters(String delimiters);
+	
+	//String useDefaultDelimiters();
+
+}
