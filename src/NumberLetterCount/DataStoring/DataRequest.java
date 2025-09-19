7dcd9812ade8478dd7ac64d870f48535ab8a62b9
Vrajpatel5123 <vrajpatel512@gmail.com>
2025-09-19 16:31:30 -0400

    fixing the datarequest error

---
+++ src/NumberLetterCount/DataStoring/DataRequest.java
@@ -0,0 +1,23 @@
+package NumberLetterCount.DataStoring;
+
+public class DataRequest {
+
+	private int number;
+
+	public DataRequest(int i) {
+		// TODO Auto-generated constructor stub
+		  int number;
+		
+	}
+	
+	public int DataRequest(int number) {
+		return this.number = number;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	
+
+}
