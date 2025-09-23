32914b882265526f66a5f0096c09776c2dfbc248
Vrajpatel5123 <vrajpatel512@gmail.com>
2025-09-23 13:44:03 -0400

    fixed the error of checkpoint

--- src/NumberLetterCount/Computing/ComputeApi.java
+++ src/NumberLetterCount/Computing/ComputeApi.java
@@ -1,7 +1,11 @@
 package numberlettercount.computing;
 
+import project.annotations.ConceptualAPI;
+
+@ConceptualAPI
 public class ComputeApi {
 
+	
 	public void insertRequest() {
 		// TODO Auto-generated method stub
 		
